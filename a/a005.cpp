7c4cc6517137320b4bcfd39c60170aef89347c34
eXcFerGodSt <excfergodst@gmail.com>
2015-10-26 21:54:22 +0800

    A005

--- a/a005.cpp
+++ a/a005.cpp
@@ -7,9 +7,9 @@ int main()
 	{
 		int a,b,c,d,e;
 		scanf("%d %d %d %d",&a,&b,&c,&d);
-		if(b-a == c-b)		//等差
+		if(b-a == c-b)		    //等差
 			{e = 2*d-c ;}
-		else if(a/b == b/c)		//等比
+		elseif(a/b == b/c)		//等比
 			{e = d*d/c ;}
 		printf("%d %d %d %d %d\n", a,b,c,d,e);
 	}
