d4f5c4a6920a296feeac7783a5e633879ae3a684
eXcFerGodSt <excfergodst@gmail.com>
2015-10-26 21:16:22 +0800

    A005

--- a/a005.cpp
+++ a/a005.cpp
@@ -1,14 +1,17 @@
-#include <iostream>
+#include <cstdio>
 int main()
 {
-	int year;
-	while(std::cin>>year)
+	int count;
+	scanf("%d",&count);
+	for (int i = 0; i < count; ++i)
 	{
-		if(year % 4 == 0 && year % 100 != 0)
-			std::cout<<"閏年"<<std::endl;
-		else if(year%400 == 0)
-			std::cout<<"閏年"<<std::endl;
-		else
-			std::cout<<"平年"<<std::endl;
+		int a,b,c,d,e;
+		scanf("%d %d %d %d",&a,&b,&c,&d);
+		if(b-a == c-b)		//等差
+			{e = 2*d-c ;}
+		else if(a/b == b/c)		//等比
+			{e = d*d/c ;}
+		printf("%d %d %d %d %d\n", a,b,c,d,e);
 	}
+
 }
